f9a2ae274e0db9f8519413a5e6551d3d087020d7
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-03-07 16:26:12 +0530

    success std

--- src/components/TpoIncomingRequest.jsx
+++ src/components/TpoIncomingRequest.jsx
@@ -97,7 +97,7 @@ function Tpodetails() {
       {incomingRequest.length >= 0 &&
         incomingRequest.map((job) => (
           <div className="dbox" key={job._id}>
-            <div className="sbox">
+            <div className="sbox">job.jobTitle
               <b>JobTitle:</b>
               {job.jobTitle}
             </div>
