10947f78c113c52a01d589ea303f91832489ee9a
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-03-14 12:25:50 +0530

    F

---
+++ src/components/Companydisplay.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Companynavbottom from "./Companynavbottom";
+import Header1 from "./Header1";
+import Footer from "./Footer";
+
+function Companydisplay() {
+  return (
+    <div>
+      <Header1 />
+
+
+
+      <Companynavbottom />
+      <Footer />
+    </div>
+  );
+}
+export default Companydisplay;
