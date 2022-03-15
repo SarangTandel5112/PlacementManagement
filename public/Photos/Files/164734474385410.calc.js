3bf613cc94eea7d677ab45d607b5562746bc81a4
Punit Tewani <tewanipunit1233@gmail.com>
2022-03-15 18:08:28 +0530

    Merege Done

---
+++ public/Photos/Files/164734474385410.calc.js
@@ -0,0 +1,20 @@
+a=5;
+b=10;
+c="+"
+result=0;
+switch(c){
+    case "+":
+        result=a+b;
+        break;
+    case "-":
+        result=a-b;
+        break;
+    case "*":
+        result=a*b;
+        break;
+    case "/":
+        result=a/b;
+        break;
+    
+}
+console.log(result);
