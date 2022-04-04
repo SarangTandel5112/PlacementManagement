93cc2ca0eca639c546f225f2f42514f6a0df8430
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-04-04 14:09:18 +0530

    Login Change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,14 +23,15 @@ function Login() {
     const finaldata = formdata;
 
     axios
-      .post("/login", finaldata,{
+      .post("/login", finaldata, {
         headers: {
           'Content-Type': 'application/json'
         },
-        withCredentials: true})
+        withCredentials: true
+      })
       .then((response) => {
         var check = response.data.err;
-       
+
 
         if (check === "incorrect username!!") {
           setvalue(check);
@@ -38,20 +39,20 @@ function Login() {
           setvalue(check);
         }
         else if (check === "student") {
-          if(response.data.user.status ==="Accpted"){
-         
+          if (response.data.user.status === "Accpted") {
+
 
             history.push("/studentHome");
-          }else if(response.data.user.status==="Rejected"){
+          } else if (response.data.user.status === "Rejected") {
             setvalue("Your Request Is Rejected By TPO! Kindly Ask Them")
           }
-          else{
-          
+          else {
+
             setvalue("Your Request is Still Pending, Please Get Approved From TPO")
           }
         } else if (check === "company") {
           history.push("/companyDashboard");
-        }else if(check==="tpo"){
+        } else if (check === "tpo") {
 
           history.push("/tpo")
         }
@@ -76,155 +77,132 @@ function Login() {
         //   history.push("/");
         // }
 
-        
+
         // if (formdata.type[0] === "tpo") {
         //   history.push("/tpo");
         // }
 
-        
+
       })
 
       .catch(() => {
         console.log("Data Has not been send, Internal Server Error");
       });
   }
-  const { email, password} = formdata;
+  const { email, password } = formdata;
   return (
 
     <div className="loginform container-fluid full-height">
-      
-      
-      <form className="lform" onSubmit={submitData}>
-        <h1 className="heading main-heading">Login to The Account</h1>
-        <div className="email container-fluid">
-          <input
-            type="text"
-            className="email1"
-            value={email}
-            name="email"
-            placeholder="Email"
-            onChange={stateChange}
-            required
-          />
-        </div>
-        <div className="password container-fluid">
-          <input
-            type="password"
-            className="password1"
-            value={password}
-            name="password"
-            placeholder="Password"
-            onChange={stateChange}
-            required
-          />
-        </div>
-        <p className="errstatus">{wrong}</p>
 
-        <div className="option">
-          <h5 className="">Login As :</h5>
-          <input
-            type="radio"
-            id="Student"
-            className="rbtn1"
-            name="type"
-            value="student"
-            onChange={stateChange}
-            required
-          />
-          <label className="rbtn">Student</label>
-          <input
-            type="radio"
-            id="Company"
-            className="rbtn1"
-            name="type"
-            value="company"
-            onChange={stateChange}
-            required
-          />
-          <label className="rbtn">Company</label>
-          <input
-            type="radio"
-            id="Tpo"
-            className="rbtn1"
-            name="type"
-            value="tpo"
-            onChange={stateChange}
-            required
-          />
-          <label className="rbtn">TPO</label>
-          <br />
-        </div>
 
-        <input
-          type="submit"
-          className="btn-primary btn-sm logbtn"
-          value="Log in"
-          name="Log in"
-        />
+      <form className="lform" >
+        <div className="loginformout">
+          <h1 className="heading main-heading">Login to The Account</h1>
+          <div className="d-block ">
+            <input
+              type="text"
+              className="form-control my-1"
+              value={email}
+              name="email"
+              placeholder="Email"
+              onChange={stateChange}
+              required
+            />
+          </div>
+          <div className="d-block ">
+            <input
+              type="password"
+              className="form-control my-1"
+              value={password}
+              name="password"
+              placeholder="Password"
+              onChange={stateChange}
+              required
+            />
+          </div>
+          <p className="errstatus">{wrong}</p>
+
+          <div className="option">
+            <h5 className="">Login As :</h5>
+            <input
+              type="radio"
+              id="Student"
+              className="rbtn1"
+              name="type"
+              value="student"
+              onChange={stateChange}
+              required
+            />
+            <label className="rbtn" htmlFor="Student">Student</label>
+            <input
+              type="radio"
+              id="Company"
+              className="rbtn1"
+              name="type"
+              value="company"
+              onChange={stateChange}
+              required
+            />
+            <label className="rbtn" htmlFor="Company">Company</label>
+            <input
+              type="radio"
+              id="Tpo"
+              className="rbtn1"
+              name="type"
+              value="tpo"
+              onChange={stateChange}
+              required
+            />
+            <label className="rbtn" htmlFor="Tpo">TPO</label>
+            <br />
+          </div>
+
+          <input
+            type="submit"
+            className="btn-primary btn-sm form-control"
+            value="Log in"
+            name="Log in"
+            onClick={submitData}
+          />
+
+
+          <div className="rform ">
+            <p className="signup">Need an Account ?</p>
+            <div className="form-row">
+              <div className="">
+                <Link to="/registercompany">
+                  <button className="btn-primary btn-sm mr-2 regbtn">Register Company</button>
+                </Link>
+                <Link to="/Studentregister">
+                  <button className="btn-primary btn-sm regbtn">Register Student</button>
+                </Link>
+              </div>
+              {/* <Link to="/registercompany">
+                <input
+                  type="submit"
+                  className="btn-primary  btn-sm regbtn mr-2"
+                  value="Register Company"
+                  name="Register Company"
+                />
+              </Link>
+
+              <Link to="/Studentregister">
+                <input
+                  type="submit"
+                  className="btn-primary btn-sm regbtn"
+                  value="Register Student"
+                  name="Register Student"
+                />
+              </Link> */}
+
+
+            </div>
+
+          </div>
+        </div>
       </form>
 
-      <div className="rform">
-        <p className="signup">Need an Account ?</p>
-        <div className="row">
-          <div className="col-lg-6 col-md-4 col-sm-3">
-            <Link to="/registercompany">
-
-              <input
-                type="submit"
-                className="btn-primary  btn-sm regbtn mr-2"
-                value="Register Company"
-                name="Register Company"
-              />
-
-            </Link>
-          </div>
-          <div className="col-lg-6 col-md-4 col-sm-3">
-            <Link to="/Studentregister">
-
-              <input
-                type="submit"
-                className="btn-primary btn-sm regbtn"
-                value="Register Student"
-                name="Register Student"
-              />
-
-
-            </Link>
-          </div>
-
-
-        </div>
-        {/* <div className="container">
-          
-            <Link to="/register">
-
-              <input
-                type="submit"
-                className="btn-primary  regbtn mr-2"
-                value="Register Company"
-                name="Register Company"
-              />
-
-            </Link>
-          
-          
-            <Link to="/register">
-
-              <input
-                type="submit"
-                className="btn-primary regbtn"
-                value="Register Student"
-                name="Register Student"
-              />
-
-
-            </Link>        
-
-
-        </div> */}
-
-      </div>
-    </div>
+    </div >
   );
 }
 
